a7db8d3d246727611c071a12cfee2d13a756b5b9
AndrewJBateman <gomezbateman@yahoo.com>
2022-05-03 18:46:49 +0200

    feat: navbar, common modules content & css added

--- angular-client/src/app/modules/common/contact/contact.module.ts
+++ angular-client/src/app/modules/common/contact/contact.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ContactRoutingModule } from './contact-routing.module';
+import { ContactComponent } from './contact.component';
 
 
 @NgModule({
-  declarations: [],
   imports: [
     CommonModule,
     ContactRoutingModule
-  ]
+  ],
+  declarations: [ContactComponent],
+  exports: [ContactComponent]
 })
 export class ContactModule { }
