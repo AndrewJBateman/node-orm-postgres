9c8f5c235db8f63d13580cab1bb800b7d1bfaf6e
AndrewJBateman <gomezbateman@yahoo.com>
2022-05-09 20:46:37 +0200

    refactor: data controllers reverted back to data

--- src/controllers/data.controller.ts
+++ src/controllers/data.controller.ts
@@ -1,24 +1,15 @@
 import { Request, Response } from "express";
-import { NorskPetroleum } from "../entity/NorskPetroleum";
+import { Data } from "../entity/Data";
 
+// CREATE ONE
 export const createData = async (req: Request, res: Response) => {
-  // try {
-  //   const { area, zone } = req.body;
-  //   const data = new NorskPetroleumgx();
-  //   data.area = area;
-  //   data.zone = zone;
-
-  //   return res.json(await data.save());
-  // } catch (error) {
-  //   if (error instanceof Error) {
-  //     return res.json(500).json({ message: error.message });
-  //   }
-  // }
-};
-
-export const getData = async (req: Request, res: Response) => {
   try {
-    return res.json(await NorskPetroleum.find());
+    const { area, zone } = req.body;
+    const data = new Data();
+    data.area = area;
+    data.zone = zone;
+
+    return res.json(await data.save());
   } catch (error) {
     if (error instanceof Error) {
       return res.json(500).json({ message: error.message });
@@ -26,45 +17,59 @@ export const getData = async (req: Request, res: Response) => {
   }
 };
 
+// READ ALL
+export const getData = async (req: Request, res: Response) => {
+  try {
+    return res.json(await Data.find());
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.json(500).json({ message: error.message });
+    }
+  }
+};
+
+// UPDATE ONE
 export const updateData = async (req: Request, res: Response) => {
   const { id } = req.params;
-  // try {
-  //   const data = await Data.findOneBy({ id: +req.params.id });
+  try {
+    const data = await Data.findOneBy({ id: +req.params.id });
 
-  //   if (!data)
-  //     return res.status(404).json({ message: "There is no data with that id" });
+    if (!data)
+      return res.status(404).json({ message: "There is no data with that id" });
 
-  //   await Data.update({ id: +id }, req.body);
-  //   return res.sendStatus(204);
-  // } catch (error) {
-  //   if (error instanceof Error) {
-  //     return res.sendStatus(500).json({ message: error.message });
-  //   }
-  // }
+    await Data.update({ id: +id }, req.body);
+    return res.sendStatus(204);
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.sendStatus(500).json({ message: error.message });
+    }
+  }
 };
 
+// DELETE ONE
 export const deleteData = async (req: Request, res: Response) => {
   const { id } = req.params;
-  // try {
-  //   return (await Data.delete({ id: +id })).affected === 0
-  //     ? res
-  //         .status(404)
-  //         .json({ message: "Matching data not found or not deleted" })
-  //     : res.sendStatus(204);
-  // } catch (error) {
-  //   if (error instanceof Error) {
-  //     return res.status(500).json({ message: error.message });
-  //   }
-  // }
+  try {
+    return (await Data.delete({ id: +id })).affected === 0
+      ? res
+          .status(404)
+          .json({ message: "Matching data not found or not deleted" })
+      : res.sendStatus(204);
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).json({ message: error.message });
+    }
+  }
 };
 
+// READ ONE
 export const getDataDetail = async (req: Request, res: Response) => {
   const { id } = req.params;
-  // try {
-  //   return res.json(await Data.findOneBy({ id: +id }));
-  // } catch (error) {
-  //   if (error instanceof Error) {
-  //     return res.status(500).json({ message: error.message });
-  //   }
-  // }
+  try {
+    return res.json(await Data.findOneBy({ id: +id }));
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).json({ message: error.message });
+    }
+  }
 };
